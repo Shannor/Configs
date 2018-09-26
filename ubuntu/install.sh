1d8a2478b5cd5090a2d9fad4e593d2971d43b454
Shannor <shannor.a.trotty@gmail.com>
2018-09-26 19:41:50 -0400

    Added auto yes

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,27 +1,26 @@
 
-sudo apt install htop
-sudo apt install ctop
-sudo apt install docker.io
-sudo apt install docker-compose
-sudo apt install golang
-# Providesd ubuntu customizations
-sudo apt install gnome-tweak-tool
+sudo apt -y install htop
+sudo apt -y install ctop
+sudo apt -y install docker.io
+sudo apt -y install docker-compose
+sudo apt -y install golang
+sudo apt -y install terminator
+# Provide-y sd ubuntu customizations
+sudo apt -y install gnome-tweak-tool
 
 # Snap installs
-sudo snap install intellij-idea-ultimate --classic
-sudo snap install vscode
-sudo snap install node
-sudo snap install kotlin
-sudo snap install postman
-sudo snap install insomnia
-sudo snap install kotlin-native
-sudo snap install slack
-sudo snap install discord
+sudo snap -y install intellij-idea-ultimate --classic
+sudo snap -y install vscode
+sudo snap -y install node
+sudo snap -y install kotlin
+sudo snap -y install postman
+sudo snap -y install insomnia
+sudo snap -y install kotlin-native
 
 
 # Setup for ZSH shell
-sudo apt-get install zsh
-sudo apt-get install git-core
+sudo apt-get -y install zsh
+sudo apt-get -y install git-core
 
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 chsh -s `which zsh`
