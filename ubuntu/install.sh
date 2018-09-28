ff832fcf4b02a031c19e29ec09680203ab88cc9e
Shannor Trotty <shannor.a.trotty@gmail.com>
2018-09-28 11:42:24 -0400

    Updated install.sh

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -7,20 +7,22 @@ sudo apt -y install golang
 sudo apt -y install terminator
 # Provide-y sd ubuntu customizations
 sudo apt -y install gnome-tweak-tool
+sudo apt -y install curl
+sudo apt -y install vim
 
 # Snap installs
-sudo snap -y install intellij-idea-ultimate --classic
-sudo snap -y install vscode
-sudo snap -y install node
-sudo snap -y install kotlin
-sudo snap -y install postman
-sudo snap -y install insomnia
-sudo snap -y install kotlin-native
+sudo snap install intellij-idea-ultimate --classic
+sudo snap install vscode
+sudo snap install node
+sudo snap install kotlin
+sudo snap install postman
+sudo snap install insomnia
+sudo snap install kotlin-native
 
 
 # Setup for ZSH shell
 sudo apt-get -y install zsh
-sudo apt-get -y install git-core
+sudo apt-get -y install git
 
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 chsh -s `which zsh`
@@ -28,5 +30,8 @@ chsh -s `which zsh`
 sudo groupadd docker
 sudo usermod -aG docker $USER
 
-# Shutdown for any restarts needed.
-sudo shutdown -r 0
+#Install GCP-cli
+export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update && sudo apt-get -y install google-cloud-sdk
