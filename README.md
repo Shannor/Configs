162af8acaaa5ed2298fa9ed84f90372d09e39033
Shannor Trotty <shannor.a.trotty@gmail.com>
2024-09-08 21:45:49 -0400

    Update README and simplify install scripts
    Enhanced the README to include more details about configurations and applications. Streamlined the Mac install script by removing redundant and outdated commands, while entirely removing Ubuntu-specific config and install scripts.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Configs
-A place to store my configs as I go for Mac and Ubuntu systems.
+A place to store my configs for setting up different applications.
+
+- VIM Setup
+- Some Homebrew apps I like
+
+
+Maybe a list of plugins for Intellij Products.
--- mac/install.sh
+++ mac/install.sh
@@ -1,29 +1,17 @@
 #!/bin/bash
-
-# TODO: Add question of if this is going to use bash or zsh.
-# TODO: Add commands to make the $GOPATH for me and add it to the env .zshrc
-
 # install home brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew update
 brew doctor
 
-brew install node
-brew install pyenv
-Brew install rbenv
-brew install jenv
+# Replacement for curl
 brew install httpie
-brew install htop
-brew install ctop
-brew install python3
+# Helps read JSON data
 brew install jq
-brew install docker-compose
 brew install golang
 brew install git
-# brew install bash-completion  #Only if bash install
 
-cp .bash_profile $HOME/.bash_profile
 # Setup for zsh and themes
 cp .zshrc $HOME/.zshrc
 brew install zsh
@@ -32,24 +20,10 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 # git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 # Scala needs Java first
-brew cask install java
-brew install scala
-brew install sbt
 
 brew cask install alfred
 brew cask install docker
-brew cask install intellij-idea
-brew cask install android-studio
-brew cask install visual-studio-code
-brew cask install authy
 brew cask install iterm2
-brew cask install postman
 brew cask install insomnia
-#  Add pyenv init to bash profile
-echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bash_profile
-echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
-echo -e 'if command -v rbenv 1>/dev/null 2>&1; then\n  eval "$(rbenv init -)"\nfi' >> ~/.zshrc
-echo 'export PATH="$HOME/.jenv/bin:$PATH"' >> ~/.bash_profile
-echo 'eval "$(jenv init -)"' >> ~/.bash_profile
 
 brew cleanup
